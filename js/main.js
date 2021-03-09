c3ab5b4f2d3f1cb4281a35f0263f435bc8b95a3c
innabatalova <batalovainna97@gmail.com>
2021-03-09 10:35:17 +0300

    Fix: work in slider

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ const swiper = new Swiper('.swiper-container', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button__next',
-    prevEl: '.slider-button__prev',
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
   },
         
 
